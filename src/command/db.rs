f9dc05c3f71101be5d3fbea27a7a33d448660f0b
Igor Bubelov <igor@bubelov.com>
2022-11-19 23:01:08 +0700

    Refactor migrations

--- src/command/db.rs
+++ src/command/db.rs
@@ -3,9 +3,26 @@ use crate::Result;
 use include_dir::include_dir;
 use include_dir::Dir;
 use rusqlite::Connection;
+use std::fmt;
 use std::fs::remove_file;
 
 static MIGRATIONS_DIR: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR/migrations");
+
+struct Migration(i16, String);
+
+impl fmt::Display for Migration {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "({}, {})",
+            self.0,
+            self.1
+                .replace("\n", "")
+                .replace("    ", "")
+                .replace(";", "; "),
+        )
+    }
+}
 
 pub fn run(args: &[String], db: Connection) -> Result<()> {
     let first_arg = match args.first() {
@@ -23,32 +40,25 @@ pub fn run(args: &[String], db: Connection) -> Result<()> {
 }
 
 pub fn migrate(db: &mut Connection) -> Result<()> {
+    execute_migrations(&get_migrations()?, db)
+}
+
+fn execute_migrations(migrations: &Vec<Migration>, db: &mut Connection) -> Result<()> {
     let mut schema_ver: i16 =
         db.query_row("SELECT user_version FROM pragma_user_version", [], |row| {
             row.get(0)
         })?;
 
-    loop {
-        let file_name = format!("{}.sql", schema_ver + 1);
-        let file = MIGRATIONS_DIR.get_file(&file_name);
-        match file {
-            Some(file) => {
-                log::warn!("Found new migration: {file_name}");
-                let sql = file.contents_utf8().ok_or(std::io::Error::new(
-                    std::io::ErrorKind::Other,
-                    format!("Can't read {file_name} in UTF-8"),
-                ))?;
-                log::warn!("Executing query:\n{sql}");
-                let tx = db.transaction()?;
-                tx.execute_batch(sql)?;
-                tx.execute_batch(&format!("PRAGMA user_version={}", schema_ver + 1))?;
-                tx.commit()?;
-                schema_ver += 1;
-            }
-            None => {
-                break;
-            }
-        }
+    let new_migrations: Vec<&Migration> =
+        migrations.iter().filter(|it| it.0 > schema_ver).collect();
+
+    for migration in new_migrations {
+        log::warn!("Found new migration: {migration}");
+        let tx = db.transaction()?;
+        tx.execute_batch(&migration.1)?;
+        tx.execute_batch(&format!("PRAGMA user_version={}", migration.0))?;
+        tx.commit()?;
+        schema_ver = migration.0;
     }
 
     log::info!("Database schema is up to date (version {schema_ver})");
@@ -65,8 +75,73 @@ fn drop(db: Connection) -> Result<()> {
     Ok(())
 }
 
+fn get_migrations() -> Result<Vec<Migration>> {
+    let mut index = 1;
+    let mut res = vec![];
+
+    loop {
+        let file_name = format!("{index}.sql");
+        let file = MIGRATIONS_DIR.get_file(&file_name);
+        match file {
+            Some(file) => {
+                let sql = file.contents_utf8().ok_or(std::io::Error::new(
+                    std::io::ErrorKind::Other,
+                    format!("Can't read {file_name} in UTF-8"),
+                ))?;
+
+                res.push(Migration(index, sql.to_string()));
+
+                index += 1;
+            }
+            None => {
+                break;
+            }
+        }
+    }
+
+    Ok(res)
+}
+
 #[cfg(test)]
 use std::sync::atomic::AtomicUsize;
 
 #[cfg(test)]
 pub static COUNTER: AtomicUsize = std::sync::atomic::AtomicUsize::new(1);
+
+#[cfg(test)]
+mod tests {
+    use std::sync::atomic::Ordering;
+
+    use super::*;
+
+    #[test]
+    fn run_migrations() {
+        let db_name = COUNTER.fetch_add(1, Ordering::Relaxed);
+        let mut db =
+            Connection::open(format!("file::testdb_{db_name}:?mode=memory&cache=shared")).unwrap();
+        let mut migrations = vec![Migration(1, "CREATE TABLE foo(bar);".into())];
+        let res = execute_migrations(&migrations, &mut db);
+        assert!(res.is_ok());
+
+        let schema_ver: i16 = db
+            .query_row("SELECT user_version FROM pragma_user_version", [], |row| {
+                row.get(0)
+            })
+            .unwrap();
+
+        assert_eq!(1, schema_ver);
+
+        migrations.push(Migration(2, "INSERT INTO foo (bar) values ('qwerty');".into()));
+
+        let res = execute_migrations(&migrations, &mut db);
+        assert!(res.is_ok());
+
+        let schema_ver: i16 = db
+            .query_row("SELECT user_version FROM pragma_user_version", [], |row| {
+                row.get(0)
+            })
+            .unwrap();
+
+        assert_eq!(2, schema_ver);
+    }
+}
