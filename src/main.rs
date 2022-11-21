6ce377b43151dda4b28ac3900340deaa916cdbfb
Igor Bubelov <igor@bubelov.com>
2022-11-21 21:55:50 +0700

    Refactor entry point

--- src/main.rs
+++ src/main.rs
@@ -13,27 +13,33 @@ use std::env;
 pub type Result<T, E = Error> = std::result::Result<T, E>;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
     init_logging();
 
-    let mut db = match command::db::open_connection() {
-        Ok(ok) => ok,
-        Err(e) => {
-            log::error!("Failed to connect to database: {e}");
-            return;
-        }
-    };
+    let mut db = command::db::open_connection()?;
 
-    if let Err(e) = command::db::migrate(&mut db) {
-        log::error!("Migration failed: {e}");
-        return;
-    }
+    command::db::migrate(&mut db)?;
 
     let args: Vec<String> = env::args().collect();
 
-    if let Err(e) = _main(&args[1..], db).await {
-        log::error!("{e}");
+    let first_arg = match args.first() {
+        Some(some) => some,
+        None => Err(Error::CLI("No actions passed".into()))?,
+    };
+
+    match first_arg.as_str() {
+        "server" => command::server::run().await?,
+        "db" => command::db::run(&args[1..], db)?,
+        "sync" => command::sync::run(db).await?,
+        "sync-users" => command::sync_users::run(db).await?,
+        "generate-report" => command::generate_report::run(db).await?,
+        "generate-android-icons" => command::generate_android_icons::run(db).await?,
+        "generate-element-categories" => command::generate_element_categories::run(db).await?,
+        "fetch-pouch-tags" => command::fetch_pouch_tags::run(db).await?,
+        first_arg => Err(Error::CLI(format!("Unknown action: {first_arg}")))?,
     }
+
+    Ok(())
 }
 
 fn init_logging() {
@@ -45,25 +51,5 @@ fn init_logging() {
         env_logger::init();
     } else {
         env_logger::builder().format_timestamp(None).init();
-    }
-}
-
-// TODO consider replacing with try block
-async fn _main(args: &[String], db: Connection) -> Result<()> {
-    let first_arg = match args.first() {
-        Some(some) => some,
-        None => Err(Error::CLI("No actions passed".into()))?,
-    };
-
-    match first_arg.as_str() {
-        "server" => command::server::run().await,
-        "db" => command::db::run(&args[1..], db),
-        "sync" => command::sync::run(db).await,
-        "sync-users" => command::sync_users::run(db).await,
-        "generate-report" => command::generate_report::run(db).await,
-        "generate-android-icons" => command::generate_android_icons::run(db).await,
-        "generate-element-categories" => command::generate_element_categories::run(db).await,
-        "fetch-pouch-tags" => command::fetch_pouch_tags::run(db).await,
-        first_arg => Err(Error::CLI(format!("Unknown action: {first_arg}"))),
     }
 }
