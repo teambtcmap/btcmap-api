8ef40ff0d8c4ed34b8a938babf41b849448dd830
Igor Bubelov <igor@bubelov.com>
2022-11-25 18:29:14 +0700

    Check if a point is inside an area

--- src/model/area.rs
+++ src/model/area.rs
@@ -10,6 +10,38 @@ pub struct Area {
     pub deleted_at: String,
 }
 
+impl Area {
+    pub fn contains(&self, lat: f64, lon: f64) -> bool {
+        let north = self.tags.get("box:north");
+        let east = self.tags.get("box:east");
+        let south = self.tags.get("box:south");
+        let west = self.tags.get("box:west");
+
+        if north.is_none() || north.unwrap().as_f64().is_none() {
+            return false;
+        }
+
+        if east.is_none() || east.unwrap().as_f64().is_none() {
+            return false;
+        }
+
+        if south.is_none() || south.unwrap().as_f64().is_none() {
+            return false;
+        }
+
+        if west.is_none() || west.unwrap().as_f64().is_none() {
+            return false;
+        }
+
+        let north = north.unwrap().as_f64().unwrap();
+        let east = east.unwrap().as_f64().unwrap();
+        let south = south.unwrap().as_f64().unwrap();
+        let west = west.unwrap().as_f64().unwrap();
+
+        lat < north && lat > south && lon > west && lon < east
+    }
+}
+
 pub static INSERT: &str = r#"
     INSERT INTO area (
         id
@@ -83,5 +115,35 @@ const fn full_mapper() -> fn(&Row) -> Result<Area> {
             updated_at: row.get(3)?,
             deleted_at: row.get(4)?,
         })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Result;
+
+    use super::Area;
+
+    #[test]
+    fn contains() -> Result<()> {
+        let tags = serde_json::json!({
+            "box:north": 49.60003042758964,
+            "box:east": -121.77932739257814,
+            "box:south": 48.81861991362668,
+            "box:west": -124.41604614257814,
+        });
+
+        let area = Area {
+            id: "".into(),
+            tags: tags,
+            created_at: "".into(),
+            updated_at: "".into(),
+            deleted_at: "".into(),
+        };
+
+        assert_eq!(area.contains(49.2623463, -123.0886088), true);
+        assert_eq!(area.contains(47.6084752, -122.3270694), false);
+
+        Ok(())
     }
 }
