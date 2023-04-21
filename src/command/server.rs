d7534a9970edca63ea28d538019f32bfde7e73f0
Igor Bubelov <igor@bubelov.com>
2023-04-21 17:37:42 +0700

    Convert access log to JSON

--- src/command/server.rs
+++ src/command/server.rs
@@ -1,20 +1,55 @@
 use crate::command;
 use crate::controller;
 use crate::Result;
+use actix_web::dev::Service;
 use actix_web::web;
 use actix_web::web::scope;
 use actix_web::{
-    middleware::{Compress, Logger, NormalizePath},
+    middleware::{Compress, NormalizePath},
     web::Data,
     App, HttpServer,
 };
+use futures_util::future::FutureExt;
+use time::OffsetDateTime;
+use tracing::info;
 
 pub async fn run() -> Result<()> {
     HttpServer::new(move || {
         App::new()
-            .wrap(Logger::new(
-                r#"%{r}a "%r" %s %b "%{Referer}i" "%{User-Agent}i" %T"#,
-            ))
+            .wrap_fn(|req, srv| {
+                let req_query_string = req.query_string().to_string();
+                let req_method = req.method().as_str().to_string();
+                let req_path = req.path().to_string();
+                let req_version = format!("{:?}", req.version());
+                let req_time = OffsetDateTime::now_utc();
+                let req_ip = req
+                    .connection_info()
+                    .peer_addr()
+                    .unwrap_or_default()
+                    .to_string();
+                let req_real_ip = req
+                    .connection_info()
+                    .realip_remote_addr()
+                    .unwrap_or_default()
+                    .to_string();
+                srv.call(req).map(move |res| {
+                    if let Ok(res) = res.as_ref() {
+                        let res_status = res.status().as_u16();
+                        info!(
+                            req_query_string,
+                            req_method,
+                            req_path,
+                            req_version,
+                            req_ip,
+                            req_real_ip,
+                            res_status,
+                            res_time_sec = (OffsetDateTime::now_utc() - req_time).as_seconds_f64(),
+                        );
+                    }
+
+                    res
+                })
+            })
             .wrap(NormalizePath::trim())
             .wrap(Compress::default())
             .app_data(Data::new(command::db::open_connection().unwrap()))
@@ -36,7 +71,7 @@ pub async fn run() -> Result<()> {
                 scope("users")
                     .service(controller::user_v2::get)
                     .service(controller::user_v2::get_by_id)
-                    .service(controller::user_v2::patch_tags)
+                    .service(controller::user_v2::patch_tags),
             )
             .service(
                 scope("areas")
@@ -55,10 +90,7 @@ pub async fn run() -> Result<()> {
                     .service(controller::report_v2::get_by_id)
                     .service(controller::report_v2::patch_tags),
             )
-            .service(
-                scope("tiles")
-                    .service(controller::tile::get),
-            )
+            .service(scope("tiles").service(controller::tile::get))
             .service(
                 scope("v2")
                     .service(
@@ -78,7 +110,7 @@ pub async fn run() -> Result<()> {
                         scope("users")
                             .service(controller::user_v2::get)
                             .service(controller::user_v2::get_by_id)
-                            .service(controller::user_v2::patch_tags)
+                            .service(controller::user_v2::patch_tags),
                     )
                     .service(
                         scope("areas")
@@ -97,10 +129,7 @@ pub async fn run() -> Result<()> {
                             .service(controller::report_v2::get_by_id)
                             .service(controller::report_v2::patch_tags),
                     )
-                    .service(
-                        scope("tiles")
-                            .service(controller::tile::get),
-                    ),
+                    .service(scope("tiles").service(controller::tile::get)),
             )
     })
     .bind(("127.0.0.1", 8000))?
