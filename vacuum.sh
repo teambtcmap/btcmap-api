04f1adc363f2d55050fccd2b25b5e0a97d9010b1
Igor Bubelov <igor@bubelov.com>
2024-01-09 15:25:58 +0700

    Update utils

--- vacuum.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sqlite3 ~/.local/share/btcmap/btcmap.db "update element set osm_json = json('{}'), tags = json('{}') where id = (select id from element where osm_json != json('{}') and deleted_at != '' limit 1);"
-
-count=$(sqlite3 ~/.local/share/btcmap/btcmap.db "select count (*) from element where deleted_at != '' and osm_json != json('{}');")
-echo "Remaining: $count"
