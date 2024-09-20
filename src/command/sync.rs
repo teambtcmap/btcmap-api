ef913d8bc0da74d7003ceaba54a7586504b34485
Igor Bubelov <igor@bubelov.com>
2024-09-20 12:57:01 +0700

    Refactor sync

--- src/command/sync.rs
+++ src/command/sync.rs
@@ -9,7 +9,6 @@ use crate::Result;
 use rusqlite::Connection;
 use rusqlite::Transaction;
 use tracing::info;
-use tracing::warn;
 
 pub async fn run(conn: &mut Connection) -> Result<()> {
     let elements = query_bitcoin_merchants().await?;
@@ -40,74 +39,7 @@ async fn merge_overpass_elements(
     }
 
     // stage 2: find and process updated elements
-    let tx: Transaction = conn.transaction()?;
-    let cached_elements = Element::select_all(None, &tx)?;
-    for fresh_element in &fresh_overpass_elements {
-        let btcmap_id = fresh_element.btcmap_id();
-        let user_id = fresh_element.uid;
-
-        match cached_elements
-            .iter()
-            .find(|it| it.overpass_data.btcmap_id() == btcmap_id)
-        {
-            Some(cached_element) => {
-                if *fresh_element != cached_element.overpass_data {
-                    info!(
-                        btcmap_id,
-                        old_json = serde_json::to_string(&cached_element.overpass_data)?,
-                        new_json = serde_json::to_string(&fresh_element)?,
-                        "Element JSON was updated",
-                    );
-
-                    if let Some(user_id) = user_id {
-                        sync::insert_user_if_not_exists(user_id, &tx).await?;
-                    }
-
-                    if fresh_element.changeset != cached_element.overpass_data.changeset {
-                        let event = Event::insert(
-                            user_id.unwrap().try_into().unwrap(),
-                            cached_element.id,
-                            "update",
-                            &tx,
-                        )?;
-                        event::service::on_new_event(&event, &tx).await?;
-                    } else {
-                        warn!("Changeset ID is identical, skipped user event generation");
-                    }
-
-                    info!("Updating osm_json");
-                    let mut updated_element =
-                        cached_element.set_overpass_data(&fresh_element, &tx)?;
-
-                    let new_android_icon = updated_element.overpass_data.generate_android_icon();
-                    let old_android_icon = cached_element
-                        .tag("icon:android")
-                        .as_str()
-                        .unwrap_or_default();
-
-                    if new_android_icon != old_android_icon {
-                        info!(old_android_icon, new_android_icon, "Updating Android icon");
-                        updated_element = Element::set_tag(
-                            updated_element.id,
-                            "icon:android",
-                            &new_android_icon.clone().into(),
-                            &tx,
-                        )?;
-                    }
-
-                    element::service::generate_issues(vec![&updated_element], &tx)?;
-                    element::service::generate_areas_mapping_old(&vec![updated_element], &tx)?;
-                }
-
-                if cached_element.deleted_at.is_some() {
-                    info!(btcmap_id, "Bitcoin tags were re-added");
-                    cached_element.set_deleted_at(None, &tx)?;
-                }
-            }
-            None => {}
-        }
-    }
-    tx.commit()?;
+    sync::sync_updated_elements(&fresh_overpass_elements, conn).await?;
 
     // stage 3: find and process new elements
     let tx: Transaction = conn.transaction()?;
--- src/sync.rs
+++ src/sync.rs
@@ -1,13 +1,14 @@
-use crate::element::Element;
-use crate::event::Event;
+use crate::element::{self, Element};
+use crate::event::{self, Event};
 use crate::osm::osm::{self, OsmElement};
+use crate::osm::overpass::OverpassElement;
 use crate::user::User;
 use crate::{discord, Error, Result};
-use rusqlite::Connection;
+use rusqlite::{Connection, Transaction};
 use serde_json::Value;
 use std::collections::{HashMap, HashSet};
 use time::OffsetDateTime;
-use tracing::{error, info};
+use tracing::{error, info, warn};
 
 /// Match fresh Overpass elements with the existing cached elements. Every
 /// cached element which isn't present in Overpass list will be marked as
@@ -97,6 +98,81 @@ pub async fn insert_user_if_not_exists(user_id: i64, conn: &Connection) -> Resul
             "User with id = {user_id} doesn't exist on OSM"
         )))?,
     };
+    Ok(())
+}
+
+pub async fn sync_updated_elements(
+    fresh_overpass_elements: &Vec<OverpassElement>,
+    conn: &mut Connection,
+) -> Result<()> {
+    let tx: Transaction = conn.transaction()?;
+    let cached_elements = Element::select_all(None, &tx)?;
+    for fresh_element in fresh_overpass_elements {
+        let btcmap_id = fresh_element.btcmap_id();
+        let user_id = fresh_element.uid;
+
+        match cached_elements
+            .iter()
+            .find(|it| it.overpass_data.btcmap_id() == btcmap_id)
+        {
+            Some(cached_element) => {
+                if *fresh_element != cached_element.overpass_data {
+                    info!(
+                        btcmap_id,
+                        old_json = serde_json::to_string(&cached_element.overpass_data)?,
+                        new_json = serde_json::to_string(&fresh_element)?,
+                        "Element JSON was updated",
+                    );
+
+                    if let Some(user_id) = user_id {
+                        insert_user_if_not_exists(user_id, &tx).await?;
+                    }
+
+                    if fresh_element.changeset != cached_element.overpass_data.changeset {
+                        let event = Event::insert(
+                            user_id.unwrap().try_into().unwrap(),
+                            cached_element.id,
+                            "update",
+                            &tx,
+                        )?;
+                        event::service::on_new_event(&event, &tx).await?;
+                    } else {
+                        warn!("Changeset ID is identical, skipped user event generation");
+                    }
+
+                    info!("Updating osm_json");
+                    let mut updated_element =
+                        cached_element.set_overpass_data(&fresh_element, &tx)?;
+
+                    let new_android_icon = updated_element.overpass_data.generate_android_icon();
+                    let old_android_icon = cached_element
+                        .tag("icon:android")
+                        .as_str()
+                        .unwrap_or_default();
+
+                    if new_android_icon != old_android_icon {
+                        info!(old_android_icon, new_android_icon, "Updating Android icon");
+                        updated_element = Element::set_tag(
+                            updated_element.id,
+                            "icon:android",
+                            &new_android_icon.clone().into(),
+                            &tx,
+                        )?;
+                    }
+
+                    element::service::generate_issues(vec![&updated_element], &tx)?;
+                    element::service::generate_areas_mapping_old(&vec![updated_element], &tx)?;
+                }
+
+                if cached_element.deleted_at.is_some() {
+                    info!(btcmap_id, "Bitcoin tags were re-added");
+                    cached_element.set_deleted_at(None, &tx)?;
+                }
+            }
+            None => {}
+        }
+    }
+    tx.commit()?;
     Ok(())
 }
 
