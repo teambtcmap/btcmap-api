3efc4941f014cb35cc287b7664ed8aea1211de3d
Igor Bubelov <igor@bubelov.com>
2025-05-25 13:18:50 +0700

    Improve error handling

--- src/rpc/auth/create_api_key.rs
+++ src/rpc/auth/create_api_key.rs
@@ -21,19 +21,22 @@ pub struct Res {
 }
 
 pub async fn run(params: Params, pool: &Pool, conf: &Conf) -> Result<Res> {
+    let error_cause_mask = "Invalid credentials";
     let start_time = OffsetDateTime::now_utc();
-    let admin = db::admin::queries_async::select_by_name(params.username, &pool).await?;
-    let password_hash = PasswordHash::new(&admin.password).unwrap();
+    let user = db::admin::queries_async::select_by_name(params.username, &pool)
+        .await
+        .map_err(|_| error_cause_mask)?;
+    let password_hash = PasswordHash::new(&user.password).map_err(|_| error_cause_mask)?;
     Argon2::default()
         .verify_password(params.password.as_bytes(), &password_hash)
-        .unwrap();
+        .map_err(|_| error_cause_mask)?;
     let token = Uuid::new_v4().to_string();
-    db::access_token::queries_async::insert(admin.id, &params.label, &token, &admin.roles, &pool)
+    db::access_token::queries_async::insert(user.id, &params.label, &token, &user.roles, &pool)
         .await?;
     let time_passed_ms = (OffsetDateTime::now_utc() - start_time).whole_milliseconds();
     let discord_message = format!(
         "User {} created a new API token labled {} ({time_passed_ms} ms)",
-        admin.name, params.label,
+        user.name, params.label,
     );
     discord::post_message(&conf.discord_webhook_api, discord_message).await;
     Ok(Res {
