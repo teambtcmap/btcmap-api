a91a0ad2dc2003d1e85cf90cd573c41f25b0498a
Igor Bubelov <igor@bubelov.com>
2026-02-02 16:44:55 +0700

    Switch boosts to lnd

--- src/rest/v4/place_boosts.rs
+++ src/rest/v4/place_boosts.rs
@@ -63,7 +63,7 @@ pub async fn post(
         ))?,
     };
     let invoice = service::invoice::create(
-        "lnbits",
+        "lnd",
         format!("element_boost:{}:{}", element.id, args.days),
         sats,
         &pool,
