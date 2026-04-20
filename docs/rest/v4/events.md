7d65b1078e902fbd10105948dd8913b54d537906
Igor Bubelov <igor@bubelov.com>
2026-04-20 20:37:56 +0700

    return event area id

--- docs/rest/v4/events.md
+++ docs/rest/v4/events.md
@@ -7,21 +7,26 @@ This document describes the endpoints for interacting with events in REST API v4
 - [Get Batch](#get-list)
 - [Get by ID](#get-by-id)
 
-### Get Bach
+### Get Batch
 
 ```bash
 curl --request GET https://api.btcmap.org/v4/events
 ```
 
-Retrieves a list of events. You can limit the output and apply a few useful filters.
+Retrieves a list of all non-deleted events with future start dates. Events without a start date are also included.
 
-#### Parameters
+#### Response Fields
 
-| Parameter | Type | Example | Default | Description |
-|-----------|------|---------|---------|-------------|
-| `updated_since` | ISO 8601 datetime | `2025-01-01T00:00:00Z` | `1970-01-01T00:00:00Z` | Filter events updated since this time. |
-| `include_past` | Boolean | `true` | `false` | Whether to include past events. |
-| `limit` | Integer | `5` | - | Limit the number of events returned. |
+| Field | Type | Description |
+|-------|------|-------------|
+| `id` | Integer | Unique identifier for the event. |
+| `area_id` | Integer or null | The area ID this event belongs to, if any. |
+| `lat` | Float | Latitude of the event location. |
+| `lon` | Float | Longitude of the event location. |
+| `name` | String | Name of the event. |
+| `website` | String | Website URL for the event. |
+| `starts_at` | ISO 8601 datetime | Start time of the event. |
+| `ends_at` | ISO 8601 datetime or null | End time of the event, if it has an end time. |
 
 #### Examples:
 
@@ -35,6 +40,7 @@ curl --request GET https://api.btcmap.org/v4/events | jq
 [
   {
     "id": 1,
+    "area_id": 1,
     "lat": 7.8812324,
     "lon": 98.3884695,
     "name": "Phuket Bitcoin Meetup",
@@ -44,6 +50,7 @@ curl --request GET https://api.btcmap.org/v4/events | jq
   },
   {
     "id": 2,
+    "area_id": null,
     "lat": 35.10219193997288,
     "lon": 129.0373886381881,
     "name": "Sats N Facts Busan",
@@ -53,6 +60,7 @@ curl --request GET https://api.btcmap.org/v4/events | jq
   },
   {
     "id": 3,
+    "area_id": 2,
     "lat": 18.782225261011515,
     "lon": 98.99429178234963,
     "name": "Weekly Bitcoin Mixer",
@@ -62,6 +70,7 @@ curl --request GET https://api.btcmap.org/v4/events | jq
   },
   {
     "id": 4,
+    "area_id": 3,
     "lat": -8.643221369429375,
     "lon": 115.14280433620284,
     "name": "Bitcoin Indonesia Conference 2025",
@@ -97,6 +106,7 @@ curl --request GET https://api.btcmap.org/v4/events/3 | jq
 ```json
 {
   "id": 3,
+  "area_id": 2,
   "lat": 18.782225261011515,
   "lon": 98.99429178234963,
   "name": "Weekly Bitcoin Mixer",
--- src/rest/v4/events.rs
+++ src/rest/v4/events.rs
@@ -14,6 +14,7 @@ use time::OffsetDateTime;
 #[derive(Serialize)]
 pub struct Item {
     pub id: i64,
+    pub area_id: Option<i64>,
     pub lat: f64,
     pub lon: f64,
     pub name: String,
@@ -28,6 +29,7 @@ impl From<Event> for Item {
     fn from(val: Event) -> Self {
         Item {
             id: val.id,
+            area_id: val.area_id,
             lat: val.lat,
             lon: val.lon,
             name: val.name,
@@ -62,4 +64,169 @@ pub async fn get_by_id(id: Path<i64>, pool: Data<MainPool>) -> RestResult<Item> 
             Error::Rusqlite(rusqlite::Error::QueryReturnedNoRows) => RestApiError::not_found(),
             _ => RestApiError::database(),
         })
+}
+
+#[cfg(test)]
+mod test {
+    use crate::db::main::test::pool;
+    use crate::{db, Result};
+    use actix_web::test::TestRequest;
+    use actix_web::web::{scope, Data};
+    use actix_web::{test, App};
+    use geojson::JsonObject;
+    use time::macros::datetime;
+    use time::OffsetDateTime;
+
+    #[test]
+    async fn get_empty_array() -> Result<()> {
+        let app = test::init_service(
+            App::new()
+                .app_data(Data::new(pool()))
+                .service(scope("/").service(super::get)),
+        )
+        .await;
+        let req = TestRequest::get().uri("/").to_request();
+        let res: Vec<JsonObject> = test::call_and_read_body_json(&app, req).await;
+        assert!(res.is_empty());
+        Ok(())
+    }
+
+    #[test]
+    async fn get_not_empty_array() -> Result<()> {
+        let pool = pool();
+        let event = db::main::event::queries::insert(
+            Some(1),
+            1.23,
+            4.56,
+            "name".to_string(),
+            "https://example.com".to_string(),
+            Some(datetime!(2099-01-01 0:00 UTC)),
+            None,
+            &pool,
+        )
+        .await?;
+        let app = test::init_service(
+            App::new()
+                .app_data(Data::new(pool))
+                .service(scope("/").service(super::get)),
+        )
+        .await;
+        let req = TestRequest::get().uri("/").to_request();
+        let res: Vec<JsonObject> = test::call_and_read_body_json(&app, req).await;
+        assert_eq!(1, res.len());
+        assert_eq!(event.id, res.first().unwrap()["id"].as_i64().unwrap());
+        assert_eq!(1, res.first().unwrap()["area_id"].as_i64().unwrap());
+        Ok(())
+    }
+
+    #[test]
+    async fn get_excludes_deleted() -> Result<()> {
+        let pool = pool();
+        let event = db::main::event::queries::insert(
+            None,
+            1.23,
+            4.56,
+            "name".to_string(),
+            "https://example.com".to_string(),
+            Some(datetime!(2099-01-01 0:00 UTC)),
+            None,
+            &pool,
+        )
+        .await?;
+        db::main::event::queries::set_deleted_at(event.id, Some(OffsetDateTime::now_utc()), &pool)
+            .await?;
+        let app = test::init_service(
+            App::new()
+                .app_data(Data::new(pool))
+                .service(scope("/").service(super::get)),
+        )
+        .await;
+        let req = TestRequest::get().uri("/").to_request();
+        let res: Vec<JsonObject> = test::call_and_read_body_json(&app, req).await;
+        assert!(res.is_empty());
+        Ok(())
+    }
+
+    #[test]
+    async fn get_excludes_past_events() -> Result<()> {
+        let pool = pool();
+        db::main::event::queries::insert(
+            None,
+            1.23,
+            4.56,
+            "past_event".to_string(),
+            "https://example.com".to_string(),
+            Some(datetime!(2020-01-01 0:00 UTC)),
+            None,
+            &pool,
+        )
+        .await?;
+        let future_event = db::main::event::queries::insert(
+            None,
+            7.89,
+            10.11,
+            "future_event".to_string(),
+            "https://example.com".to_string(),
+            Some(datetime!(2099-01-01 0:00 UTC)),
+            None,
+            &pool,
+        )
+        .await?;
+        let app = test::init_service(
+            App::new()
+                .app_data(Data::new(pool))
+                .service(scope("/").service(super::get)),
+        )
+        .await;
+        let req = TestRequest::get().uri("/").to_request();
+        let res: Vec<JsonObject> = test::call_and_read_body_json(&app, req).await;
+        assert_eq!(1, res.len());
+        assert_eq!(
+            future_event.id,
+            res.first().unwrap()["id"].as_i64().unwrap()
+        );
+        Ok(())
+    }
+
+    #[test]
+    async fn get_by_id() -> Result<()> {
+        let pool = pool();
+        let event = db::main::event::queries::insert(
+            Some(1),
+            1.23,
+            4.56,
+            "name".to_string(),
+            "https://example.com".to_string(),
+            Some(datetime!(2099-01-01 0:00 UTC)),
+            None,
+            &pool,
+        )
+        .await?;
+        let app = test::init_service(
+            App::new()
+                .app_data(Data::new(pool))
+                .service(super::get_by_id),
+        )
+        .await;
+        let req = TestRequest::get().uri("/1").to_request();
+        let res: JsonObject = test::call_and_read_body_json(&app, req).await;
+        assert_eq!(event.id, res["id"].as_i64().unwrap());
+        assert_eq!(1, res["area_id"].as_i64().unwrap());
+        Ok(())
+    }
+
+    #[test]
+    async fn get_by_id_not_found() -> Result<()> {
+        let pool = pool();
+        let app = test::init_service(
+            App::new()
+                .app_data(Data::new(pool))
+                .service(super::get_by_id),
+        )
+        .await;
+        let req = TestRequest::get().uri("/999").to_request();
+        let res = test::call_service(&app, req).await;
+        assert_eq!(res.status(), 404);
+        Ok(())
+    }
 }
