f6b8327a7b5fcad1f368e6fa3d93be025932bbfd
Igor Bubelov <igor@bubelov.com>
2026-06-30 15:52:03 +0700

    Remove steak and shake osm query exception

--- src/service/overpass.rs
+++ src/service/overpass.rs
@@ -13,13 +13,9 @@ static API_URL: &str = "https://overpass-api.de/api/interpreter";
 static USER_AGENT_VALUE: &str = "btcmap-api/1.0 (https://btcmap.org)";
 
 static QUERY: &str = r#"
-    [out:json][timeout:300];
-    area["name"="United States"]->.boundaryarea;
-    (
-      nwr["currency:XBT"=yes];
-      way["brand:wikidata"="Q7605233"]["disused:amenity"!~"."](area.boundaryarea);
-    );
-    out meta geom;
+[out:json][timeout:300];
+nwr["currency:XBT"=yes];
+out meta geom;
 "#;
 
 #[derive(Serialize, Deserialize)]
